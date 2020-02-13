6aabfdc4fd28dc2fe18859f1abd48e2a1a4da2d5
Jefwillems <jefwillems100@gmail.com>
2020-02-13 11:33:20 +0100

    form can now be posted and validated correctly

--- qr_kit/forms.py
+++ qr_kit/forms.py
@@ -5,8 +5,9 @@ class DynamicQrForm(forms.Form):
 
     def __init__(self, values_to_fill: dict = None, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        for field_name, field_value in values_to_fill.items():
-            print(field_name, field_value)
-            self.fields[field_name] = forms.CharField(label=field_name, initial=field_value,
-                                                      disabled=field_value is not None)
+        if values_to_fill is not None:
+            for field_name, field_value in values_to_fill.items():
+                print(field_name, field_value)
+                self.fields[field_name] = forms.CharField(label=field_name, initial=field_value,
+                                                          )
+                self.fields[field_name].widget.attrs['readonly'] = field_value is not None
--- qr_kit/views.py
+++ qr_kit/views.py
@@ -1,13 +1,16 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, FormView
+from django.views.generic.edit import FormMixin
+from django.shortcuts import get_object_or_404
 from qr_kit.models import QrCode, Category
 from qr_kit.forms import DynamicQrForm
-from django.shortcuts import get_object_or_404
 
 
-class QrCodeView(DetailView):
+class QrCodeView(DetailView, FormMixin):
     template_name = 'qr_kit/qr_code.html'
     queryset = QrCode.objects.all()
     context_object_name = 'qr_code'
+    success_url = ''
+    form_class = DynamicQrForm
 
     def get_object(self, queryset=None) -> QrCode:
         uuid = self.kwargs.get('uuid')
@@ -31,3 +34,23 @@ class QrCodeView(DetailView):
         context['form'] = form
 
         return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        # noinspection PyAttributeOutsideInit
+        self.object = self.get_object()
+        values_to_fill = self.object.category.values_to_fill.all()
+        names = [x.name for x in values_to_fill]
+        print(request.POST)
+        values_to_fill = dict([(name, request.POST.get(name, None)) for name in names])
+        form = DynamicQrForm(values_to_fill=values_to_fill, data=request.POST)
+        if form.is_valid():
+            print('valid')
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        return self.render_to_response(context={'form': form})
+
+    def form_invalid(self, form):
+        return self.render_to_response(context={'form': form})
