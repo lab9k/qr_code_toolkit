7b07e4ed0fb6074ad2c9b06a69cedb2d6e9eddf9
Jefwillems <jefwillems100@gmail.com>
2020-08-06 15:37:25 +0200

    move distancecalculation to locationMixin.js

--- client/mixins/locationMixin.js
+++ client/mixins/locationMixin.js
@@ -10,5 +10,19 @@ export const LocationMixin = {
     return {
       location: null
     }
+  },
+  methods: {
+    distanceBetween(latA, lonA, latB, lonB) {
+      const R = 6371e3
+      const φ1 = (latA * Math.PI) / 180
+      const φ2 = (latB * Math.PI) / 180
+      const Δφ = ((latB - latA) * Math.PI) / 180
+      const Δλ = ((lonB - lonA) * Math.PI) / 180
+      const a =
+        Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2)
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+      return R * c
+    }
   }
 }
