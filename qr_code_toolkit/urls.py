a01c57274f820f6821c19791f13153980cb486d3
Jefwillems <jefwillems100@gmail.com>
2020-09-10 14:01:22 +0200

    update packages

--- qr_code_toolkit/urls.py
+++ qr_code_toolkit/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
